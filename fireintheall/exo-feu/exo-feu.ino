d857229b6dd9119ef68d6c194c5b805183eb76d2
antoine zazzera <antoine.zazzera@influans.com>
2016-04-26 15:20:23 +0200

    2 fire in the hall

--- fireintheall/exo-feu/exo-feu.ino
+++ fireintheall/exo-feu/exo-feu.ino
@@ -11,6 +11,7 @@ int timeGreen;
 int timeOrange;
 
 int timeloul = 0;
+boolean switchColor = true;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,38 +24,63 @@ void setup() {
     pinMode(LED_LEFT_O, OUTPUT);
     pinMode(LED_LEFT_V, OUTPUT);
 
-    timeGreen = 5000;    
-    timeRed = 10000;
+    timeGreen = 5000;
     timeOrange = 2000;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_LEFT_V,HIGH);
-  digitalWrite(LED_RIGHT_R,HIGH);
-  delay(timeGreen);
-  digitalWrite(LED_LEFT_V,LOW);
-  digitalWrite(LED_LEFT_O,HIGH);
-  delay(timeOrange);
-  digitalWrite(LED_O,LOW);
-  digitalWrite(LED_R,HIGH);
-  delay(timeRed);
-  digitalWrite(LED_R,LOW);
-  timeloul++;
+  if(switchColor){
+    digitalWrite(LED_LEFT_V,HIGH);
+    digitalWrite(LED_RIGHT_R,HIGH);
+    delay(timeGreen);
+    digitalWrite(LED_LEFT_V,LOW);
+    digitalWrite(LED_LEFT_O,HIGH);
+    delay(timeOrange);
+    digitalWrite(LED_RIGHT_R,LOW);
+    digitalWrite(LED_LEFT_O,LOW);
+  } else {
+    digitalWrite(LED_RIGHT_V,HIGH);
+    digitalWrite(LED_LEFT_R,HIGH);
+    delay(timeGreen);
+    digitalWrite(LED_RIGHT_V,LOW);
+    digitalWrite(LED_RIGHT_O,HIGH);
+    delay(timeOrange);
+    digitalWrite(LED_LEFT_R,LOW);
+    digitalWrite(LED_RIGHT_O,LOW);
+  }
   
-  if (timeloul == 5)
+  timeloul++;
+  switchColor = !switchColor;
+
+
+  
+  
+  if (timeloul == 2)
   {
     for (int i = 0; i < 50; i++)
     {
-      digitalWrite(LED_V,HIGH);
+      digitalWrite(LED_LEFT_V,HIGH);
       delay(200);
-      digitalWrite(LED_V,LOW);
-      digitalWrite(LED_O,HIGH);
+      digitalWrite(LED_LEFT_V,LOW);
+      digitalWrite(LED_LEFT_O,HIGH);
       delay(200);
-      digitalWrite(LED_O,LOW);
-      digitalWrite(LED_R,HIGH);
+      digitalWrite(LED_LEFT_O,LOW);
+      digitalWrite(LED_LEFT_R,HIGH);
       delay(200);
-      digitalWrite(LED_R,LOW);
+      digitalWrite(LED_LEFT_R,LOW);
+      delay(200);
+
+      
+      digitalWrite(LED_RIGHT_V,HIGH);
+      delay(200);
+      digitalWrite(LED_RIGHT_V,LOW);
+      digitalWrite(LED_RIGHT_O,HIGH);
+      delay(200);
+      digitalWrite(LED_RIGHT_O,LOW);
+      digitalWrite(LED_RIGHT_R,HIGH);
+      delay(200);
+      digitalWrite(LED_RIGHT_R,LOW);
     }
 
     timeloul = 0;
