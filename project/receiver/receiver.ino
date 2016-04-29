1d7e125e89a21572e0691bee2aff499f089abc51
Gitdefllo <mail.de.florent@gmail.com>
2016-04-29 06:06:47 +0200

    XBee: receiver, sender, I2C: communication between raspberry and arduino

---
+++ project/receiver/receiver.ino
@@ -0,0 +1,23 @@
+//Xbee receiver
+//This example code is in the Public Domain
+
+int sentDat;
+
+void setup() {
+  Serial.begin(9600);   
+  pinMode(2, OUTPUT); 
+}
+
+void loop() {
+  Serial.println("test");
+  if (Serial.available() > 0) {
+  sentDat = Serial.read(); 
+
+  //if(sentDat == 'h'){
+          //activate the pumpkin for one second and then stop
+      digitalWrite(2, HIGH);
+          delay(1000);
+          digitalWrite(2, LOW);
+  //}
+  }
+}
