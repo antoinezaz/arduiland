3da6dca047d3b9e5e04b04b7ff9877917336220b
antoine zazzera <antoine.zazzera@influans.com>
2016-04-29 13:53:23 +0200

    limit the max temperature

--- project/VentiloMolo/app/src/main/java/com/cassone/devmalin/ventilomolo/RegisterActivity.java
+++ project/VentiloMolo/app/src/main/java/com/cassone/devmalin/ventilomolo/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cassone.devmalin.ventilomolo.models.Id;
 import com.cassone.devmalin.ventilomolo.models.Setting;
@@ -32,29 +33,34 @@ public class RegisterActivity extends AppCompatActivity {
         findViewById(R.id.edit_register).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TemperatureService temperatureService = SetupTempService.setupTempService();
-                Id id = new Id();
-                id.set$oid("5723140afca0fd7cf0f2736c");
-                Setting setting = new Setting();
-                setting.set_id(id);
-                setting.setMax_temp(Integer.valueOf(((EditText) findViewById(R.id.edit_max_temp)).getText().toString()));
-                Call<JSONObject> call = temperatureService.registerUser(TemperatureService.API_KEY, setting);
-                call.enqueue(new Callback<JSONObject>() {
-                    @Override
-                    public void onResponse(Call<JSONObject> call, Response<JSONObject> response) {
-                        if (!response.isSuccess()) {
-                            Log.d("Register", "error");
-                        } else {
-                            Log.d("Register", "success");
-                            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                int max_temp = Integer.valueOf(((EditText) findViewById(R.id.edit_max_temp)).getText().toString());
+                if(max_temp > 60 || max_temp < -10){
+                    Toast.makeText(getApplicationContext(), "please enter value between -10 and 60", Toast.LENGTH_SHORT).show();
+                } else{
+                    TemperatureService temperatureService = SetupTempService.setupTempService();
+                    Id id = new Id();
+                    id.set$oid("5723140afca0fd7cf0f2736c");
+                    Setting setting = new Setting();
+                    setting.set_id(id);
+                    setting.setMax_temp(max_temp);
+                    Call<JSONObject> call = temperatureService.registerUser(TemperatureService.API_KEY, setting);
+                    call.enqueue(new Callback<JSONObject>() {
+                        @Override
+                        public void onResponse(Call<JSONObject> call, Response<JSONObject> response) {
+                            if (!response.isSuccess()) {
+                                Log.d("Register", "error");
+                            } else {
+                                Log.d("Register", "success");
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<JSONObject> call, Throwable t) {
-                        Log.d("Register", "Failure" + t.toString());
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<JSONObject> call, Throwable t) {
+                            Log.d("Register", "Failure" + t.toString());
+                        }
+                    });
+                }
             }
         });
     }
