8caacc57185cc5247eea7d39d1da3d417bd4f643
antoine zazzera <antoine.zazzera@influans.com>
2016-04-29 13:52:52 +0200

    limit the max temperature

--- project/raspland/thermometer.py
+++ project/raspland/thermometer.py
@@ -1,10 +1,10 @@
 import os
-import requests
+import requests, json
 #comand = '{"celcius":"10","isMotorActived":"false","humidity":"50"}'
 #os.system('curl -i -X POST -H "Content-Type:application/json" -d '+comand+' https://api.mlab.com/api/1/databases/arduiland/collections/Temperature?apiKey=e16CW-yUW4cxtREIjflDYuVWANP6V70W')
 
 r = requests.get("https://api.mlab.com/api/1/databases/arduiland/collections/Setting/5723140afca0fd7cf0f2736c?apiKey=e16CW-yUW4cxtREIjflDYuVWANP6V70W")
-print(r.text)
-
+test = json.loads(r.text)
+print(test['max_temp'])
 
 
